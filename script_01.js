a2ed0f79c4c8bb0c21b1ff54ddff4bd020811395
Peter Weber <peter-aalen@gmx.net>
2023-04-06 14:46:37 +0200

    Better End oben

--- script_01.js
+++ script_01.js
@@ -12,6 +12,41 @@
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
+
+output(getSentence(["Ich","bin","Max"],"S"));
+output(getSentence(["Bist","du","Max"],"Q"));
+output(getSentence(["Ich","bin"],"E"));
+output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+
+function getSentence(arr, op) {
+    const GAP = " ";
+    const PUNCT = getPunct(op);
+    let str = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i == arr.length-1) {
+            str += arr[i] + PUNCT;
+        } else {
+            str += arr[i] + GAP;  
+        }           
+    }
+     return str;
+}
+
+function getPunct(op) {
+    switch (op) {
+        case "S":
+            return ".";
+        case "Q":
+            return "?";
+        case "E":
+            return "!";
+        default:
+            return "#"    
+    }
+}
+
+
 // output(getSentence1(["Ich","bin","Max"],"S"));
 // output(getSentence2(["Bist","du","Max"],"Q"));
 // output(getSentence3(["Ich","bin"],"E"));
