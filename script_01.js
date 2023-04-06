3e5c114acfa99fef318e9e6992c374bdd75d48f4
Peter Weber <peter-aalen@gmx.net>
2023-04-06 13:31:55 +0200

    Challenge Finished

--- script_01.js
+++ script_01.js
@@ -12,11 +12,73 @@
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-output(getSentence(["Ich","bin","Max"],"S"));
-output(getSentence(["Bist","du","Max"],"Q"));
-output(getSentence(["Ich","bin"],"E"));
-output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+// output(getSentence1(["Ich","bin","Max"],"S"));
+// output(getSentence2(["Bist","du","Max"],"Q"));
+// output(getSentence3(["Ich","bin"],"E"));
+// output(getSentence4(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
 
+function getSentence1(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i == arr.length-1) {
+            str += arr[i] + PUNCT;
+        } else {
+            str += arr[i] + GAP;  
+        }           
+    }
+     return str;
+}
+
+
+function getSentence2(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i == arr.length-1) {
+            str += arr[i] + PUNCT;
+        } else {
+            str += arr[i] + GAP;  
+        }           
+    }
+     return str;
+}
+
+
+function getSentence3(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i == arr.length-1) {
+            str += arr[i] + PUNCT;
+        } else {
+            str += arr[i] + GAP;  
+        }           
+    }
+     return str;
+}
+
+
+function getSentence4(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i == arr.length-1) {
+            str += arr[i] + PUNCT;
+        } else {
+            str += arr[i] + GAP;  
+        }           
+    }
+     return str;
+}
 
 
 // Modul: Ausgabe in Konsole : Test
